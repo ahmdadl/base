4885075240509929addec0c814e9b5a87d9f0469
ahmed adel <abo3adel35@gmail.com>
2019-07-29 10:52:22 +0200

    Initial commit

---
+++ src/Bootstrap.php
@@ -0,0 +1,15 @@
+<?php declare (strict_types=1);
+
+namespace App;
+
+use Whoops;
+use Symfony\Component\HttpFoundation\{
+    Request,
+    Response
+};
+
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+
+
+
+
