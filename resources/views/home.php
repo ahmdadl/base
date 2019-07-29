8cafee9cb71cbe13c4fde5551b99f5d3c0e025f1
ahmed adel <abo3adel35@gmail.com>
2019-07-29 13:15:59 +0200

    add views

---
+++ resources/views/home.php
@@ -0,0 +1,3 @@
+<?php $this->layout('layout', ['title' => 'Home Page']);?>
+
+<h1><?=$this->e($name)?></h1>
---
+++ resources/views/layout.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?=$this->e($title)?></title>
+    <link href="assets/css/app.css" rel="stylesheet" type="text/css">
+    <link rel="icon" href="">
+</head>
+<body>
+    <?=$this->section('content')?>
+    <script src="assets/js/app.js"></script>
+</body>
+</html>
