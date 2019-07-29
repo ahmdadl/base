91746e9fbda182d329924f00bacf5a534720c89e
ahmed adel <abo3adel35@gmail.com>
2019-07-29 13:26:13 +0200

    downgrade plates to v3

--- src/Controllers/Home.php
+++ src/Controllers/Home.php
@@ -6,20 +6,26 @@ use Symfony\Component\HttpFoundation\{
     Request,
     Response
 };
-
+use League\Plates\Engine;
 class Home
 {
     private $request;
     private $response;
+    private $view;
 
-    public function __construct(Request $request, Response $response)
-    {
+    public function __construct(
+        Request $request,
+        Response $response,
+        Engine $view
+    ) {
         $this->request = $request;
         $this->response = $response;
+        $this->view = $view;
     }
 
     public function show($params = [])
     {
-        $this->response->setContent('<h1>Ahmed Adel</h1>');
+        $html = $this->view->render('home', ['name' => 'ahmed']);
+        $this->response->setContent($html);
     }
 }
