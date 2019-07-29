7b08f43db8be762b913222c7f51e265e252a14ca
ahmed adel <abo3adel35@gmail.com>
2019-07-29 18:35:39 +0200

    update addnew form

--- resources/views/addnew.php
+++ resources/views/addnew.php
@@ -1,5 +1,19 @@
 <?php $this->layout('layout', ['title' => 'Add new Joke'])?>
-<form action="" method="post" class="form pure-form">
-    <input type='text' />
-    <button type="submit" class="pure-button pure-button-primary">Save</button>
+<h1>
+    <hr>
+</h1>
+<form action="/ft/public/addNew" method="post" class="form pure-form pure-form-stacked">
+    <fieldset>
+        <legend>Add new Joke</legend>
+    
+        <!-- <label for="text">Joke Text</label> -->
+        <input id="text" type='text' name="text" placeholder="Write Joke ..." />
+        <select name="author">
+            <option>Select Author</option>
+            <option value="0">abo3adel35</option>
+            <option value="1">someoneElse</option>
+        </select>
+        <button type="submit" name="submit" class="pure-button pure-button-primary">Save</button>
+    </fieldset>
 </form>
+
