af175b9f326dc552b4166ffcaa6dcc1b2af3a3e9
ahmed adel <abo3adel35@gmail.com>
2019-07-29 18:04:34 +0200

    add addnew joke frm

---
+++ resources/views/addnew.php
@@ -0,0 +1,5 @@
+<?php $this->layout('layout', ['title' => 'Add new Joke'])?>
+<form action="" method="post" class="form pure-form">
+    <input type='text' />
+    <button type="submit" class="pure-button pure-button-primary">Save</button>
+</form>
