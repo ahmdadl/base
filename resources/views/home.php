1ffad4574e64f4572032062a073e0d1b685a1ab4
ahmed adel <abo3adel35@gmail.com>
2019-07-29 16:39:06 +0200

    add button styles to pure

--- resources/views/home.php
+++ resources/views/home.php
@@ -1,9 +1,29 @@
 <?php $this->layout('layout', ['title' => 'Home Page']);?>
 
 <h1><?=$this->es($name)?></h1>
-<?php $this->start('list') ?>
-    <ul>
-        <li>at</li>
-        <li>section</li>
-    </ul>
-<?php $this->stop('list') ?>
+
+<table class="pure-table pure-table-striped pure-table-horizontal">
+        <thead>
+            <tr>
+                <th>#</th>
+                <th>Author</th>
+                <th>Content</th>
+                <th>Opts</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>01</td>
+                <td>abo3adel</td>
+                <td>asdhj asd kasjdk samdlsa k</td>
+                <td>
+                    <button type="button" class="pure-button pure-button-primary">
+                        <a href='/frame/public/edit?id=1'>Edit</a>
+                    </button>
+                    <button type="button" class="pure-button button-error">
+                        <a href='/frame/public/delete?id=1'>Delete</a>
+                    </button>
+                </td>
+            </tr>
+        </tbody>
+    </table>
--- resources/views/partials/nav.php
+++ resources/views/partials/nav.php
@@ -1,10 +1,8 @@
-<nav class="nav navbar navbar-primary bg-primary">
-    <ul>
-        <li <?=$this->uri('/', 'class="active"')?>>
-            <a href='/ft/public/'>Home</a>
-        </li>
-        <li <?=$this->uri('/about', 'class="active"')?>>
-            <a href='/ft/public/about'>About</a>
+<nav class="home-menu pure-menu pure-menu-horizontal">
+    <a class="pure-menu-heading pure-menu-link <?= $this->uri('/', 'active')?>" href='/ft/public/'>Home</a>
+    <ul class="pure-menu-list">
+        <li class="pure-menu-item <?=$this->uri('/about', 'class="active"')?>">
+            <a class="pure-menu-link" href='/ft/public/about'>About</a>
         </li>
     </ul>
 </nav>
