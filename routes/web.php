e52d7453935ae158baf22beca39a7a98376fdf7b
ahmed adel <abo3adel35@gmail.com>
2019-07-29 17:47:41 +0200

    set addnew route

--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,6 @@ use FastRoute\RouteCollector;
 return function (RouteCollector $r) {
     $uri = '/ft/public';
     $r->get($uri . '/', ['App\Controllers\Home::show']);
+    $r->get($uri . '/addNew', ['App\Controllers\Joke::addNew']);
 };
 
