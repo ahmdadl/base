dc5b910c831b5b683772aa856944196fd049ecb2
ahmed adel <abo3adel35@gmail.com>
2019-07-29 16:38:42 +0200

    remove unused classes

--- src/Controllers/Home.php
+++ src/Controllers/Home.php
@@ -3,26 +3,19 @@
 namespace App\Controllers;
 
 use Symfony\Component\HttpFoundation\{
-    Request,
-    Response
+    Request
 };
-// use League\Plates\Engine;
-// use League\Plates\Extension\Asset;
-
 use App\View\FrontRenderInterface;
 class Home
 {
     private $request;
-    private $response;
     private $view;
 
     public function __construct(
         Request $request,
-        Response $response,
         FrontRenderInterface $view
     ) {
         $this->request = $request;
-        $this->response = $response;
         $this->view = $view;
     }
 
