299a3125cac498481242bbb95e63dcd496691938
ahmed adel <abo3adel35@gmail.com>
2019-07-29 11:10:07 +0200

    update services

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -10,6 +10,20 @@ use Symfony\Component\HttpFoundation\{
 
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
+define('IS_DEBUG', true);
 
+// register the error handler
+$whoops = new Whoops\Run;
+if (IS_DEBUG) {
+    ini_set('display_errors', '1');
+    ini_set('display_startup_errors', '1');
+    error_reporting(E_ALL);
+    $whoops->pushHandler(new Whoops\Handler\PrettyPageHandler);
+} else {
+    $whoops->pushHandler(function ($e) {
+        echo 'TODO: Friendly error page and send email to admin';
+    });
+}
+$whoops->register();
 
 
