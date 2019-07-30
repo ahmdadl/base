a42b8d7e5ae5cf14746ccd109e803713a6d1bea4
ahmed adel <abo3adel35@gmail.com>
2019-07-30 14:14:03 +0200

    fix edit route

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,8 @@ return function (RouteCollector $r) {
     $uri = '/ft/public';
     $r->get($uri . '/', ['App\Controllers\Home::show']);
     $r->addRoute(['GET', 'POST'], $uri . '/addNew', ['App\Controllers\Joke::addNew']);
-    $r->addRoute(['GET, POST'], $uri . '/edit/j/{id}', [
-        'App\Controllers\Joke::edit',
+    $r->addRoute(['GET', 'POST'], $uri . '/edit/j/{id}', [
+        'App\Controllers\Joke::edit'
     ]);
 };
 
