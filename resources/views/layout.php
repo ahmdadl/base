f429db7764553532b5448439fb5b055d308eea38
ahmed adel <abo3adel35@gmail.com>
2019-07-30 14:13:44 +0200

    add base href

--- resources/views/layout.php
+++ resources/views/layout.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <base href="/ft/public/" />
     <title><?=$this->e($title)?></title>
     <link href="<?=$this->asset('assets/css/app.css')?>" rel="stylesheet" type="text/css">
     <link rel="icon" href="">
