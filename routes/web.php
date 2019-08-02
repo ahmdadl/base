e055c05b1492d6097ebdb231449d9f0cf888aef2
ahmed adel <abo3adel35@gmail.com>
2019-08-02 23:27:16 +0200

    add middleware Auth to home

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,8 @@ use FastRoute\RouteCollector;
 
 return function (RouteCollector $r) {
     $uri = '/ft/public';
-    $r->get($uri . '/', ['App\Controllers\Home::show']);
+    $r->get($uri . '/', ['App\Controllers\Home::show',
+    'middlewares' => ['Auth',]]);
     $r->addRoute(['GET', 'POST'], $uri . '/addNew', ['App\Controllers\Joke::addNew']);
     $r->addRoute(['GET', 'POST'], $uri . '/edit/j/{id}', [
         'App\Controllers\Joke::edit'
