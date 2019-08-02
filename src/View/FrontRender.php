44b5e67ef8eb148f1a39fe506b4cab8467dc22b3
ahmed adel <abo3adel35@gmail.com>
2019-08-02 23:46:18 +0200

    make sessions avaliable to all views

--- src/View/FrontRender.php
+++ src/View/FrontRender.php
@@ -6,6 +6,7 @@ use Symfony\Component\HttpFoundation\{
     Request,
     Response
 };
+use App\Util\AppSession;
 use League\Plates\Engine;
 use League\Plates\Extension\{
     Asset,
@@ -18,15 +19,18 @@ class FrontRender implements FrontRenderInterface
     private $requset;
     private $response;
     private $view;
+    private $session;
 
     public function __construct(
         Request $request,
         Response $response,
-        Engine $view
+        Engine $view,
+        AppSession $session
     ) {
         $this->request = $request;
         $this->response = $response;
         $this->view = $view;
+        $this->session = $session;
         $this->configView();
     }
 
@@ -54,6 +58,20 @@ class FrontRender implements FrontRenderInterface
         $this->view->registerFunction('es', function($str) {
             return htmlspecialchars(strip_tags(trim($str)), ENT_QUOTES);
         });
+
+        // create method function like laravel one
+        $this->view->registerFunction('_method', function ($me) {
+            return '<input type="hidden" name="_method" value="' . $me . '" />';
+        });
+
+
+        // assign data to all views
+        // check if there is user sign in or not
+        $this->view->addData(['hasSession' => $this->request->hasPreviousSession()]);
+
+        // make session available to all views
+        $this->view->addData(['session' => $this->session]);
+        
 
     }
 }
