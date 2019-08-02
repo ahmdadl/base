7c08b12efe900d1bed690e38c913a91557891a1b
ahmed adel <abo3adel35@gmail.com>
2019-08-02 23:30:18 +0200

    create initial middleware

--- src/Controllers/Home.php
+++ src/Controllers/Home.php
@@ -7,6 +7,7 @@ use App\View\FrontRenderInterface;
 use DB\Model\HomeModel;
 use Hashids\Hashids;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use App\Util\AppSession;
 
 // use DB\Model\UserModel;
 
@@ -23,17 +24,23 @@ class Home
         FrontRenderInterface $view,
         HomeModel $model,
         Hashids $hashid,
-        SessionInterface $session
+        AppSession $session
     ) {
         $this->request = $request;
         $this->view = $view;
         $this->model = $model;
         $this->hashid = $hashid;
         $this->session = $session;
+        if ($this->request->hasPreviousSession()){
+            $this->session->sessStart();
+        }
     }
 
     public function show($params = []) : void
     {
+        // var_dump($this->request->hasPreviousSession());
+        // var_dump($this->session);
+        // var_dump(ini_get('session.use_strict_mode'));
         $this->view->render('home', [
             'name' => 'not me',
             'data' => $this->model->readAll(),
--- src/Middlewares/Auth.php
+++ src/Middlewares/Auth.php
@@ -4,7 +4,7 @@ namespace App\Middlewares;
 
 class Auth
 {
-    public function proccess()
+    public function process()
     {
         return ['errCode' => false];
     }
