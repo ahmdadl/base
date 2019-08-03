2fb552de70a737ee25a1198747a15207759f0885
ahmed adel <abo3adel35@gmail.com>
2019-08-03 13:09:05 +0200

    add login route

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,8 @@ return function (RouteCollector $r) {
     $r->addRoute(['POST', 'DELETE'], $uri. '/delete/j/{id}', [
         'App\Controllers\Joke::delete',
     ]);
-    $r->get($uri . '/logIn', [
+    $r->get($uri . '/logIn', ['App\Controllers\User::logIn']);
+    $r->post($uri . '/logIn', [
         'App\Controllers\User::logIn',
         'middlewares' => ['Auth']
     ]);
