0780635bc668a2e41b09b8d31db6d9b72ba334a8
ahmed adel <abo3adel35@gmail.com>
2019-08-03 22:19:19 +0200

    create csrf token

--- src/Middlewares/CsrfVerify.php
+++ src/Middlewares/CsrfVerify.php
@@ -4,14 +4,32 @@ namespace App\Middlewares;
 
 use Symfony\Component\HttpFoundation\Request;
 
-class CsrfVerify extends Base
+class CsrfVerify extends Base implements MiddlewareInterface
 {
-    public function process()
+    /**
+     * all documention at Base Class
+     *
+     * @return bool
+     */
+    public function process() : bool
     {
+        // get saved in session csrf token
+        $known = parent::$session->se->get('X_CSRF_TOKEN');
+        // get the token submited with user input
         $token = parent::$request->request->get('csrfToken');
-        var_dump($token);
-        if (hash_equals('asdasd', $token)) {
+        // check if the two tokens not equal
+        if (!hash_equals($known, $token)) {
+            // send message to view via flash sessions
+            parent::$session->se->getFlashBag()->add(
+                'danger',
+                'an error occured. please try again later'
+            );
+
+            // stop any other middleware
             return true;
         }
+
+        // if no error
+        return false;
     }
 }
