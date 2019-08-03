75f1ad5c4d1e49a3a75fcc4c7cc8d1b9103dd019
ahmed adel <abo3adel35@gmail.com>
2019-08-03 09:23:09 +0200

    add login link to navbar

--- resources/views/partials/nav.php
+++ resources/views/partials/nav.php
@@ -2,7 +2,14 @@
     <a class="pure-menu-heading pure-menu-link <?= $this->uri('/', 'active')?>" href='/ft/public/'>Home</a>
     <ul class="pure-menu-list">
         <li class="pure-menu-item <?=$this->uri('/about', 'class="active"')?>">
-            <a class="pure-menu-link" href='/ft/public/about'>About</a>
+            <a class="pure-menu-link" href='about'>About</a>
+        </li>
+        <li class="pure-menu-item">
+        <?php if (!$hasSession) : ?>
+                <a class="pure-menu-link pure-button button-success" href='logIn'>SignIn</a>
+        <?php else : ?>
+                <a class="pure-menu-link pure-button button-danger" href='/ft/public/logOut'>SignOut</a>
+        <?php endif?>
         </li>
     </ul>
 </nav>
