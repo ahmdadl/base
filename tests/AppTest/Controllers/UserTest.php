29a3786413488f294f9016f87c34d93a74a23def
ahmed adel <abo3adel35@gmail.com>
2019-08-13 04:27:52 +0200

    test instance

--- tests/AppTest/Controllers/UserTest.php
+++ tests/AppTest/Controllers/UserTest.php
@@ -3,15 +3,33 @@
 namespace AppTest\Controllers;
 
 use App\Controllers\User;
+use DB\Model\UserModel;
 use PHPUnit\Framework\TestCase;
+use Mockery;
 
 final class UserTest extends TestCase
 {
     use ControllersTrait;
 
-    public function testSome() : void
+    private $user;
+
+    public function setUp() : void
     {
-        self::$session->sessStart();
-        $this->assertTrue(true);
+        $this->user = Mockery::mock(UserModel::class);
+    }
+
+    public function testInstance() : void
+    {
+        $ctrl = new User(
+            self::$request,
+            $this->user,
+            self::$view,
+            self::$session,
+            self::$hashids
+        );
+
+        $this->assertTrue(
+            Mockery::type(User::class)->match($ctrl)
+        );
     }
 }
