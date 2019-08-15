f54b5f2695ccedbfe07809b3fe12eb756a9a1a0c
ahmed adel <abo3adel35@gmail.com>
2019-08-15 05:55:57 +0200

    fix  base uri

--- routes/web.php
+++ routes/web.php
@@ -3,20 +3,8 @@
 use FastRoute\RouteCollector;
 
 return function (RouteCollector $r) {
-    $uri = '/ft/public';
+    $uri = '/fc/public';
     $r->get($uri . '/', ['Home@show',
     'middlewares' => []]);
-    $r->addRoute(['GET', 'POST'], $uri . '/addNew', ['Joke@addNew']);
-    $r->addRoute(['GET', 'POST'], $uri . '/edit/j/{id}', [
-        'Joke@edit'
-    ]);
-    $r->addRoute(['POST', 'DELETE'], $uri. '/delete/j/{id}', [
-        'Joke@delete',
-    ]);
-    $r->get($uri . '/logIn', ['User@logIn']);
-    $r->post($uri . '/logIn', [
-        'User@logIn',
-        'middlewares' => ['CsrfVerify']
-    ]);
 };
 
