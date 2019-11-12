4c1618e384ff915473a9b6a3234cb2253f24167b
ahmed adel <abo3adel35@gmail.com>
2019-11-12 16:08:33 +0200

    rename home cntrl

---
+++ src/Controllers/HomeController.php
@@ -0,0 +1,44 @@
+<?php declare (strict_types=1);
+
+namespace App\Controllers;
+
+use Symfony\Component\HttpFoundation\Request;
+use App\View\FrontRenderInterface;
+use App\Models\HomeModel;
+use Hashids\Hashids;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use App\Util\AppSession;
+use Symfony\Component\HttpFoundation\Response;
+
+// use DB\Model\UserModel;
+
+class HomeController
+{
+    private $request;
+    private $view;
+    private $model;
+    private $hashid;
+    private $session;
+
+    public function __construct(
+        Request $request,
+        FrontRenderInterface $view,
+        HomeModel $model,
+        Hashids $hashid,
+        AppSession $session
+    ) {
+        $this->request = $request;
+        $this->view = $view;
+        $this->model = $model;
+        $this->hashid = $hashid;
+        $this->session = $session;
+        $this->session->sessStart();
+    }
+
+    public function show($params = [])
+    {
+        return $this->view->render('home', [
+            'name' => 'not me',
+        ]);
+    }
+}
