5bc2ae8afdc95dde5a9709c307d4ab48b2373a7c
ahmed adel <abo3adel35@gmail.com>
2019-11-13 14:36:04 +0200

    handle latest browser meta

--- resources/views/layouts/base.php
+++ resources/views/layouts/base.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title><?=$this->e($title)?></title>
     <link href="<?=$this->asset('/assets/css/app.css')?>" rel="stylesheet" type="text/css">
     <link rel="icon" href="">
