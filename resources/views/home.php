801095312fde124deb8fec8a9731ade43c47b544
ahmed adel <abo3adel35@gmail.com>
2019-11-13 14:43:47 +0200

    styling my name

--- resources/views/home.php
+++ resources/views/home.php
@@ -11,7 +11,7 @@
     <canvas id='canvas' style=''></canvas>
     <div class="container d-flex h-100 align-items-center">
         <div class="mx-auto text-center">
-            <h1 class="mx-auto my-0 text-uppercase">Hello, I`m Ahmed Adel</h1>
+            <h1 class="mx-auto my-0 text-capitalize">Hello, I`m <span class='text-danger'>Ahmed Adel</span></h1>
             <h2 class="text-white-50 mx-auto mt-2 mb-5">and I`m</h2>
             <h1 id=''>
                 <span id='job-title'>A Full Stack Web Developer</span>
