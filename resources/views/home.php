13111a7c8ba099eb95616f7317de9b9906c1901a
ahmed adel <abo3adel35@gmail.com>
2019-11-14 00:16:10 +0200

    styling skills title

--- resources/views/home.php
+++ resources/views/home.php
@@ -15,7 +15,7 @@
             <h2 class="text-white-50 mx-auto mt-2 mb-5">and I`m</h2>
             <h1 id=''>
                 <span id='job-title'></span>
-                    <animated-job-title></animated-job-title>
+                <animated-job-title></animated-job-title>
                 </span>
                 <span id='blink' class='blink'>|</span>
             </h1>
@@ -31,15 +31,30 @@
     </h2>
     <div class="row">
         <?php foreach ($pros as $p) : ?>
-        <div class="pros col-6 col-md-4 shadow mt-4">
-            <div class="content p-1">
-                <div class="fa-3x d-inline-block bg-primary p-2 text-light w-25 h-25 mx-auto hexagon hexagon1">
-                    <i class="fas fa-<?=$p->icon?>"></i>
+            <div class="pros col-6 col-md-4 shadow mt-4">
+                <div class="content p-1">
+                    <div class="fa-3x d-inline-block bg-primary p-2 text-light w-25 h-25 mx-auto hexagon hexagon1">
+                        <i class="fas fa-<?= $p->icon ?>"></i>
+                    </div>
+                    <h3 class="d-block mt-3"><?= $p->title ?></h3>
+                    <span class="text-muted"><?= $p->txt ?></span>
                 </div>
-                <h3 class="d-block mt-3"><?=$p->title?></h3>
-                <span class="text-muted"><?=$p->txt?></span>
             </div>
-        </div>
         <?php endforeach ?>
+    </div>
+</section>
+
+<section id='skills' class="skills bg-light text-dark text-center mt-3">
+    <h2>
+        Skills
+        <hr width="40%" class='mx-auto bg-dark pt-1 rounded ' />
+    </h2>
+    <div class="row">
+        <div class="col-12 col-md-6">
+
+        </div>
+        <div class="col-12 col-md-6">
+            
+        </div>
     </div>
 </section>
