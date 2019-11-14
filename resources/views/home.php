0342fc3878588a3786041f0774fa35e82b476eaa
ahmed adel <abo3adel35@gmail.com>
2019-11-14 15:52:58 +0200

    fix styling card as col

--- resources/views/home.php
+++ resources/views/home.php
@@ -84,24 +84,26 @@
             <hr class='mx-auto bg-light pt-1 rounded w-25 px-5' />
         </h2>
         <div class="text-center mt-5">
-            <div class="card-group">
+            <div class="row">
                 <?php foreach ($projects as $p) : ?>
-                    <div class="project card bg-light text-white shadow border border-dark">
-                        <div class='card-body position-relative p-0'>
-                            <img src="<?= $this->asset('assets/img/' . $p->img) ?>" class="card-img" alt="title">
-                            <div ref='cardOverlay' class="card-img-overlay">
-                                <h5 class="card-title">Client: <?= $p->client ?></h5>
-                                <p class="card-text"><?= $p->info ?></p>
+                    <div class='col-12 col-md-6 col-lg-4 mb-3 px-sm-5'>
+                        <div class="project card bg-light text-white shadow border border-dark">
+                            <div class='card-body position-relative p-0'>
+                                <img src="<?= $this->asset('assets/img/' . $p->img) ?>" class="card-img" alt="title">
+                                <div ref='cardOverlay' class="card-img-overlay">
+                                    <h5 class="card-title">Client: <?= $p->client ?></h5>
+                                    <p class="card-text"><?= $p->info ?></p>
+                                </div>
                             </div>
-                        </div>
-                        <div class="card-footer text-dark">
-                            <h5 class="card-title"><?= $p->title ?></h5>
-                            <div class="tags d-block text-capitalize">
-                                <?php foreach ($p->tags as $tag) : ?>
-                                    <span class="badge badge-primary p-1 mx-2">
-                                        <?= $tag ?>
-                                    </span>
-                                <?php endforeach ?>
+                            <div class="card-footer text-dark">
+                                <h5 class="card-title"><?= $p->title ?></h5>
+                                <div class="tags d-block text-capitalize">
+                                    <?php foreach ($p->tags as $tag) : ?>
+                                        <span class="badge badge-primary p-1 mx-2">
+                                            <?= $tag ?>
+                                        </span>
+                                    <?php endforeach ?>
+                                </div>
                             </div>
                         </div>
                     </div>
