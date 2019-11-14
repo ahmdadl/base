de141c44d7214651984e78c2d54926f95596e4f2
ahmed adel <abo3adel35@gmail.com>
2019-11-15 01:14:41 +0200

    add csrftoken input to base

--- resources/views/layouts/base.php
+++ resources/views/layouts/base.php
@@ -1,19 +1,23 @@
 <!DOCTYPE html>
-<html lang="<?=$session->get('lang') ?? ''?>">
+<html lang="<?= $session->get('lang') ?? '' ?>">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title><?=$this->e($title)?></title>
-    <link href="<?=$this->asset('/assets/css/app.css')?>" rel="stylesheet" type="text/css">
+    <title><?= $this->e($title) ?></title>
+    <link href="<?= $this->asset('/assets/css/app.css') ?>" rel="stylesheet" type="text/css">
     <link rel="icon" href="">
 </head>
+
 <body>
-    <?=$this->insert('partials/nav', ['navClass' => $navClass ?? ''])?>
-    <main class="<?=$mainClass ?? 'container-fluid'?>" id="<?=$id ?? 'body'?>">
-    <?=$this->section('content')?>
+    <?= $this->insert('partials/nav', ['navClass' => $navClass ?? '']) ?>
+    <main class="<?= $mainClass ?? 'container-fluid' ?>" id="<?= $id ?? 'body' ?>">
+        <?= $this->section('content') ?>
+        <input type="hidden" ref='csrf_token' id='csrf_token' class="d-none" name='csrf_token' value="<?= $session->get('X_CSRF_TOKEN') ?>" />
     </main>
-    <script src='<?=$this->asset('/assets/js/bootstrap-native-v4.min.js')?>'></script>
-    <script src="<?=$this->asset('/assets/js/app.js')?>"></script>
+    <script src='<?= $this->asset('/assets/js/bootstrap-native-v4.min.js') ?>'></script>
+    <script src="<?= $this->asset('/assets/js/app.js') ?>"></script>
 </body>
+
 </html>
