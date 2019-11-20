5e5504acf03f54d73188e0e0926b2af931626224
ahmed adel <abo3adel35@gmail.com>
2019-11-20 22:08:46 +0200

    init model value to old data

--- resources/views/post/create.php
+++ resources/views/post/create.php
@@ -10,12 +10,14 @@
 </header>
 
 <div class='createPost mt-5'>
-    <form ref='createPostForm' class="form needs-validation" :class="{'was-validated': h.d.titleErr || h.d.bodyErr}" action='/blog/post/' method="post" @submit.stop.prevent="h.d.beforeSubmit" novalidate>
+    <h1><?= $errors->any() ? 'good' : 'err' ?></h1>
+    <?php print_r($session->getFlashBag()->peekAll()) ?>
+    <form ref='createPostForm' class="form needs-validation <?= !$errors->any() ?: 'was-validated' ?>" :class="{'was-validated': h.d.titleErr || h.d.bodyErr}" action='/blog/posts' method="post" @submit.stop.prevent="h.d.beforeSubmit" enctype="multipart/form-data" novalidate>
         <?= $this->csrf() ?>
         <div class="form-group row">
             <label for="title" class="col-sm-2 col-form-label">Title</label>
             <div class="col-sm-10">
-                <input type="text" class="form-control" id="title" placeholder="post title" v-model='h.d.title' :class="{'is-invalid': h.d.titleErr, 'is-valid': false === h.d.titleErr}" minlength="5" required />
+                <input type="text" class="form-control <?= $errors->has('title') ? 'is-invalid' : 'is-valid' ?>" id="title" placeholder="post title" v-model.lazy.trim='h.d.title' name='title' :class="{'is-invalid': h.d.titleErr, 'is-valid': false === h.d.titleErr}" v-init:title="'<?= $errors->getOld('title') ?>'" minlength="10" required />
                 <div class="invalid-feedback">
                     title must be withen 25 and 255 chars
                 </div>
@@ -25,8 +27,19 @@
             <label for="img" class="col-sm-2 col-form-label">Image</label>
             <div class="col-sm-10">
                 <div class="custom-file">
-                    <input type="file" @change="h.d.handleFile" class="custom-file-input" id="customFile" accept="image/*" ref="img">
+                    <input type="file" name='img' @change="h.d.handleFile" class="custom-file-input <?= $session->getFlashBag()->has('danger') ? 'is-invalid' : '' ?>" id="customFile" accept="image/*" required />
                     <label class="custom-file-label" for="customFile">Choose file</label>
+                    <div class="invalid-feedback">
+                        <?php
+                        if ($errors->has('files')) {
+                            foreach ($errors->get('files') as $m) {
+                                echo $m->files->type ? 'File type is not supported' : 'File Size must be less than 500 KiloByte';
+                            }
+                        }
+                        
+                        ?>
+                    </div>
+                    <?php print_r($errors->get('files'))?>
                 </div>
             </div>
         </div>
@@ -40,15 +53,16 @@
         <div class="form-group row">
             <label for="body" class="col-sm-2 col-form-label">Body</label>
             <div class="col-sm-10">
-                <textarea class="form-control" :class="{'is-invalid': h.d.bodyErr, 'is-valid': false === h.d.bodyErr}" id="body" placeholder="write in markdown" v-model='h.d.body' rows="15" minlength='25' required></textarea>
+                <textarea class="form-control <?= $errors->has('body') ? 'is-invalid' : 'is-valid' ?>" :class="{'is-invalid': h.d.bodyErr, 'is-valid': false === h.d.bodyErr}" id="body" placeholder="write in markdown" v-model.trim.lazy='h.d.body' v-init:body="'<?= $errors->getOld('body') ?>'" name='body' rows="15" minlength='25' required></textarea>
                 <div class="invalid-feedback">
                     must be required
                 </div>
+                
             </div>
         </div>
         <div class="form-group row">
             <div class="col-10 offset-sm-2">
-                <button type="submit" class='btn btn-primary btn-block text-capitalize'>
+                <button type="submit" name='save' class='btn btn-primary btn-block text-capitalize'>
                     <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
                     save post
                 </button>
@@ -56,3 +70,10 @@
         </div>
     </form>
 </div>
+
+
+<!-- 
+<form action='/blog/posts' method="post">
+<input type='text' name='www' />
+<button type='submit' >asdw</button>
+</form> -->
