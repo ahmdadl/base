a19cfe7c54c2414f47294caf818cbddcbad9d5f7
ahmed adel <abo3adel35@gmail.com>
2019-11-23 00:00:17 +0200

    handle lang files on about section

--- resources/lang/en/index.php
+++ resources/lang/en/index.php
@@ -14,7 +14,41 @@ return [
         'hello' => 'Hello, I`m ',
         'myName' => 'Ahmed Adel',
         'and' => 'and I`m',
-        'start' => 'Get Started'
+        'start' => 'Get Started',
+        'title' => [
+            'about' => 'About'
+        ],
+        'sec' => [
+            'feat' => [
+                'Responsive' => [
+                    'title' => 'Responsive',
+                    'txt' => 'My layouts will work on any device, big or small.'
+                ],
+                'Dynamic' => [
+                    'title' => 'Dynamic',
+                    'txt' => 'I love making pages come to life.'
+                ],
+                'Tested' => [
+                    'title' => 'Tested',
+                    'txt' => 'All website features is well tested before deployment'
+                ],
+                'Organized' => [
+                    'title' => 'Organized, Documented',
+                    'icon' => 'code',
+                    'txt' => 'To help other`s  to continue after me.'
+                ],
+                'Up_To_Date' => [
+                    'title' => 'Up to Date',
+                    'icon' => 'laptop-code',
+                    'txt' => 'using always the latest versions of my tools.'
+                ],
+                'Multi_Language' => [
+                    'title' => 'Multi Language',
+                    'icon' => 'language',
+                    'txt' => 'TestIt in this website itself.'
+                ],
+            ]
+        ]
     ],
     'cpost' => [
         'title' => 'Create Post',
@@ -32,7 +66,7 @@ return [
             'titleReq' => 'The Title must be greater than 15 characters.',
             'imgType' => 'The Image must be a file of type: png, jpg, jpeg.',
             'imgSize' => 'The Image must be less than 750 kilobytes.',
-            'bodyReq' => 'The Body must be greater than 150 characters.' 
+            'bodyReq' => 'The Body must be greater than 150 characters.'
         ]
     ]
 ];
--- resources/views/home/about.php
+++ resources/views/home/about.php
@@ -1,6 +1,6 @@
 <section id='about' class='about bg-light text-dark text-center mt-5 pt-2'>
     <h2>
-        About
+        <?=$this->__('home.title.about')?>
         <hr class='mx-auto bg-dark pt-1 rounded w-25 px-5' />
     </h2>
     <div class="row">
@@ -10,8 +10,8 @@
                     <div class='wyg pointer btn btn-outline-primary btn-lg fa-3x rounded'>
                         <i class="fas fa-<?= $p->icon ?>"></i>
                     </div>
-                    <h3 class="d-block mt-3"><?= $p->title ?></h3>
-                    <span class="text-muted"><?= $p->txt ?></span>
+                    <h3 class="d-block mt-3"><?= $this->__('home.sec.feat.' . $p->title . '.title') ?></h3>
+                    <span class="text-muted"><?= $this->__('home.sec.feat.' . $p->title . '.txt') ?></span>
                 </div>
             </div>
         <?php endforeach ?>
