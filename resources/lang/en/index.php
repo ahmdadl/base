d36390dc1bfb6b891e7d3a185d9252f502bcb8be
ahmed adel <abo3adel35@gmail.com>
2019-11-22 23:33:46 +0200

    handle create post lang files

--- resources/lang/en/index.php
+++ resources/lang/en/index.php
@@ -10,4 +10,23 @@ return [
         'register' => 'Register',
         'logout' => 'LogOut'
     ],
+    'cpost' => [
+        'title' => 'Create Post',
+        'inp' => [
+            'title' => 'Title',
+            'img' => 'Image',
+            'body' => 'Body',
+            'btn' => 'Save Post'
+        ],
+        'ph' => [
+            'enter' => 'Enter ',
+            'file' => 'Choose File'
+        ],
+        'vaild' => [
+            'titleReq' => 'The Title must be greater than 15 characters.',
+            'imgType' => 'The Image must be a file of type: png, jpg, jpeg.',
+            'imgSize' => 'The Image must be less than 750 kilobytes.',
+            'bodyReq' => 'The Body must be greater than 150 characters.' 
+        ]
+    ]
 ];
