20687e4215a597a9b3a7b1d0c8807cf163f8f49b
ahmed adel <abo3adel35@gmail.com>
2019-11-28 22:15:10 +0200

    save comment

--- src/Controllers/CommentController.php
+++ src/Controllers/CommentController.php
@@ -30,15 +30,22 @@ class CommentController extends BaseController
 
     public function store()
     {
+        $postId = $this->post('postId');
         $name = Filter::filterStr($this->post('name'));
         $email = Filter::filterStr($this->post('email'));
         $message = Filter::filterStr($this->post('message'));
 
         $errors = new class {
+            public $pid = false;
             public $name = false;
             public $email = false;
             public $message = false;
+            public $done = false;
         };
+
+        if (null === $postId) {
+            $errors->pid = true;
+        }
 
         if (!$name || !Filter::len($name, 5, 255)) {
             $errors->name = true;
@@ -51,9 +58,20 @@ class CommentController extends BaseController
         if (!$message || !Filter::len($message, 5)) {
             $errors->message = true;
         }
+    
+        if ($name && $email && $message && !$errors->pid) {
+            // save comment
+            $this->model->postId = (int) $postId;
+            $this->model->name = $name;
+            $this->model->email = $email;
+            $this->model->body = $message;
 
-        if ($name && $email && $message) {
-            
+            if ($this->model->save()) {
+                $errors->done = true;
+            }
         }
+
+        header('Content-Type: application/json');
+        echo json_encode($errors);
     }
 }
--- src/Models/Comment.php
+++ src/Models/Comment.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use App\DbConfig\MySqli;
+
 class Comment
 {
     private $con;
@@ -14,4 +16,30 @@ class Comment
     public $email;
     public $body;
     public $created_at;
+
+    public function __construct (MySqli $db)
+    {
+        $this->con = $db->getConnection();
+    }
+
+    // public function __set($name, $value)
+    // {
+    //     $this->{$name} = Filter::filterStr($value);
+    // }
+
+    public function save ()
+    {
+        $stmt = 'INSERT INTO ' . $this->tbName . ' (postId, name, email, body) VALUES (:pid, :name, :email, :body)';
+
+        $sql = $this->con->prepare($stmt);
+
+        $params = [
+            ':pid' => 1,
+            ':name' => $this->name,
+            ':email' => $this->email,
+            ':body' => $this->body
+        ];
+
+        return ($sql->execute($params));
+    }
 }
