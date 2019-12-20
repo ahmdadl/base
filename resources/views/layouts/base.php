9e1cfc5739fa1cfff95cbb48891edc8c64918cf9
ahmed adel <abo3adel35@gmail.com>
2019-12-21 01:14:34 +0200

    fix social icon stick to it`s parent

--- resources/views/layouts/base.php
+++ resources/views/layouts/base.php
@@ -31,20 +31,20 @@
             <div class="container">
                 <div class="row text-center py-4">
                     <div class="mx-auto w-75">
-                        <a href='https://github.com/abo3adel' target="_blank" class="btn btn-outline-primary btn-brand transition mr-2">
+                        <a href='https://github.com/abo3adel' target="_blank" class="btn btn-outline-primary btn-brand transition mr-2 mt-2">
                             <i class='fab fa-github'></i>
                         </a>
-                        <a href='https://codepen.io/abo3adel' target="_blank" class="btn btn-outline-danger btn-brand transition mr-2">
+                        <a href='https://codepen.io/abo3adel' target="_blank" class="btn btn-outline-danger btn-brand transition mr-2 mt-2">
 
                             <i class='fab fa-codepen'></i>
                         </a>
-                        <a href='https://www.linkedin.com/in/ahmed-adel-30a932119/' target="_blank" class="btn btn-outline-info btn-brand transition mr-2">
+                        <a href='https://www.linkedin.com/in/ahmed-adel-30a932119/' target="_blank" class="btn btn-outline-info btn-brand transition mr-2 mt-2">
                             <i class='fab fa-linkedin-in'></i>
                         </a>
-                        <a href='https://fb.com/a7md200' target="_blank" class="btn btn-outline-primary btn-brand transition mr-2">
+                        <a href='https://fb.com/a7md200' target="_blank" class="btn btn-outline-primary btn-brand transition mr-2 mt-2">
                             <i class='fab fa-facebook-f'></i>
                         </a>
-                        <a href='https://wa.me/201143647417' target="_blank" class="btn btn-outline-success btn-brand transition mr-2">
+                        <a href='https://wa.me/201143647417' target="_blank" class="btn btn-outline-success btn-brand transition mr-2 mt-2">
                             <i class='fab fa-whatsapp'></i>
                         </a>
                     </div>
